8d45fdad0b9adabf1b2f9ce24edf7e3d877bd0a8
bhaskar <bhaskardutta2209@gmail.com>
2020-08-15 21:07:08 +0530

    Adding payment system

---
+++ workflows/src/main/java/com/template/flows/IssueCoin.java
@@ -0,0 +1,112 @@
+package com.template.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import com.r3.corda.lib.accounts.contracts.states.AccountInfo;
+import com.r3.corda.lib.accounts.workflows.UtilitiesKt;
+import com.r3.corda.lib.accounts.workflows.flows.RequestKeyForAccount;
+import com.template.contracts.CoinContract;
+import com.template.states.CoinState;
+import net.corda.core.contracts.Command;
+import net.corda.core.contracts.StateAndRef;
+import net.corda.core.flows.*;
+import net.corda.core.identity.AnonymousParty;
+import net.corda.core.identity.Party;
+import net.corda.core.node.services.vault.QueryCriteria;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+// ******************
+// * Initiator flow *
+// ******************
+@InitiatingFlow
+@StartableByRPC
+public class IssueCoin extends FlowLogic<Void> {
+
+    private final String sender;
+    private final String receiver;
+    private final double value;
+
+    public IssueCoin(String sender, String receiver, double value) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.value = value;
+    }
+
+    @Suspendable
+    @Override
+    public Void call() throws FlowException {
+        // Initiator flow logic goes here.
+        if(getOurIdentity().getName().getOrganisation().equalsIgnoreCase("Bank")) {
+
+            AccountInfo senderAccountInfo = UtilitiesKt.getAccountService(this).accountInfo(sender).get(0).getState().getData();
+            AccountInfo receiverAccountInfo = UtilitiesKt.getAccountService(this).accountInfo(receiver).get(0).getState().getData();
+
+            AnonymousParty senderParty = subFlow(new RequestKeyForAccount(senderAccountInfo));
+            AnonymousParty receiverParty = subFlow(new RequestKeyForAccount(receiverAccountInfo));
+
+            FlowSession receiverSession = initiateFlow(receiverAccountInfo.getHost());
+
+            Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
+
+            Command command = new Command(new CoinContract.Issue(),Arrays.asList(senderParty.getOwningKey(),receiverParty.getOwningKey()));
+
+            UUID id = receiverAccountInfo.getIdentifier().getId();
+            QueryCriteria.VaultQueryCriteria criteria = new QueryCriteria.VaultQueryCriteria().withExternalIds(Arrays.asList(id));
+
+            List<StateAndRef<CoinState>> coinStates = getServiceHub().getVaultService().queryBy(CoinState.class,criteria).getStates();
+
+
+            TransactionBuilder txB = new TransactionBuilder(notary)
+                    .addCommand(command);
+
+            double newValue = value;
+
+            if(coinStates.size() > 0) {
+                newValue += coinStates.stream().mapToDouble(it -> it.getState().getData().getValue()).sum();
+                coinStates.stream().forEach(it -> txB.addInputState(it));
+            }
+
+            CoinState outputState = new CoinState(newValue,sender,senderParty,receiver,receiverParty);
+            txB.addOutputState(outputState);
+
+            SignedTransaction selfSignedTx = getServiceHub().signInitialTransaction(txB,senderParty.getOwningKey());
+            final SignedTransaction fullySignedTx = subFlow(new CollectSignaturesFlow(selfSignedTx,
+                    Arrays.asList(receiverSession), Collections.singleton(senderParty.getOwningKey())));
+
+            SignedTransaction stx = subFlow(new FinalityFlow(fullySignedTx,Arrays.asList(receiverSession)));
+
+        } else {
+            throw new FlowException("Only Bank can issue Coins");
+        }
+        return null;
+    }
+}
+
+@InitiatedBy(IssueCoin.class)
+class IssueCoinResponder extends FlowLogic<String> {
+
+    private FlowSession counterpartySession;
+
+    public IssueCoinResponder(FlowSession counterpartySession) {
+        this.counterpartySession = counterpartySession;
+    }
+
+    @Suspendable
+    @Override
+    public String call() throws FlowException {
+        subFlow(new SignTransactionFlow(counterpartySession) {
+            @Override
+            protected void checkTransaction(@NotNull SignedTransaction stx) throws FlowException {
+
+            }
+        });
+        return subFlow(new ReceiveFinalityFlow(counterpartySession)).toString();
+    }
+}
+
